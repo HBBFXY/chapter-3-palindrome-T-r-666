e8907213fd2ef88076032a0284577802341ff1af
T-r-666 <1154730956@qq.com>
2025-09-20 10:35:47 +0800

    完成回文数的判断作业

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # 这里编写你的代码
 num=input("请输入一个5位数字：")
 if len(num)!=5 or not num.isdigit():
-    print("错误：请输入一个5位数字")
+    print("错误,请输入一个5位数字")
 else:
-    # 判断是否为回文数
     if num==num[::-1]:
         print("是回文数")
     else:
